f15957af135f4a01eb27810c65934de7ca64e2e8
Ripan <ripanrediansyah@gmail.com>
2026-04-10 22:21:13 +0700

    Fix project ID in SQL table creation query
    Updated table creation query to use the correct project ID.

--- Query Analisis Tabel.sql
+++ Query Analisis Tabel.sql
@@ -12,7 +12,7 @@ SELECT
     p.product_name,
     p.price AS actual_price,
     t.discount_percentage,
-    -- Logika Laba
+    -- Logika Laba berdasarkan harga obat --
     CASE
         WHEN p.price <= 50000 THEN 0.10
         WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
@@ -20,9 +20,9 @@ SELECT
         WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
         ELSE 0.30
     END AS persentase_gross_laba,
-    -- Nett Sales (Harga setelah diskon)
+    -- Nett Sales (Harga setelah diskon) --
     (p.price * (1 - t.discount_percentage)) AS nett_sales,
-    -- Nett Profit (Laba Kotor - Nominal Diskon)
+    -- Nett Profit (Laba Kotor - Nominal Diskon) --
     ((p.price * (CASE 
         WHEN p.price <= 50000 THEN 0.10
         WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
@@ -31,6 +31,6 @@ SELECT
         ELSE 0.30 
     END)) - (p.price * t.discount_percentage)) AS nett_profit,
     t.rating AS rating_transaksi
-FROM `project_id.kf_analytics.kf_final_transaction` t
-LEFT JOIN `project_id.kf_analytics.kf_kantor_cabang` c ON t.branch_id = c.branch_id
-LEFT JOIN `project_id.kf_analytics.kf_product` p ON t.product_id = p.product_id;
+FROM `rakamin-kf-analytics-01062024.kf_analytics.kf_final_transaction` t
+LEFT JOIN `rakamin-kf-analytics-01062024.kf_analytics.kf_kantor_cabang` c ON t.branch_id = c.branch_id
+LEFT JOIN `rakamin-kf-analytics-01062024.kf_analytics.kf_product` p ON t.product_id = p.product_id;
